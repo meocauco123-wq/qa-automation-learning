dfd386acbbb2b78881d765b49de943781243be43
Phuong Phan <phuongphan@Phuongs-MacBook-Air.local>
2026-03-16 12:45:52 +0700

    fix api test log

---
+++ tests/api.test.js
@@ -0,0 +1,20 @@
+const https = require("https");
+
+console.log("Start API test");
+
+https.get("https://jsonplaceholder.typicode.com/posts/1", (res) => {
+
+  console.log("STATUS:", res.statusCode);
+
+  if (res.statusCode === 200) {
+    console.log("TEST PASS");
+    process.exit(0);
+  } else {
+    console.log("TEST FAIL");
+    process.exit(1);
+  }
+
+}).on("error", (err) => {
+  console.log("ERROR:", err.message);
+  process.exit(1);
+});
